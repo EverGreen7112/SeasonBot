72b7c2ac9aba6096bdf37d354614a652d28563f9
EverGreen7112 <34937866+EverGreen7112@users.noreply.github.com>
2018-02-09 12:40:57 +0200

    key bindings

--- src/org/usfirst/frc/team7112/robot/OI.java
+++ src/org/usfirst/frc/team7112/robot/OI.java
@@ -20,6 +20,17 @@ public class OI {
 	Button buttonB = new JoystickButton(JS, 2); //
 	Button buttonLB = new JoystickButton(JS, 5); //
 	Button buttonRB = new JoystickButton(JS, 6); //
+	public Joystick DrivingJS = new Joystick(1);
+	Button buttonTrigger = new JoystickButton(DrivingJS, 1);
+	Button buttonThumb = new JoystickButton(DrivingJS, 2);
+	Button button5 = new JoystickButton(DrivingJS, 5);
+	Button button6 = new JoystickButton(DrivingJS, 6);
+	Button button7 = new JoystickButton(DrivingJS, 7);
+	Button button8 = new JoystickButton(DrivingJS, 8);
+	Button button9 = new JoystickButton(DrivingJS, 9);
+	Button button10 = new JoystickButton(DrivingJS, 10);
+	Button button11 = new JoystickButton(DrivingJS, 11);
+	Button button12 = new JoystickButton(DrivingJS, 12);
 	
 	//creates an instance of OI
 	public static final void init() {
@@ -38,6 +49,47 @@ public class OI {
 	public Button Get_RB_Button(){
 		return buttonRB;
 	}
+	
+	public Button GetButtonTrigger(){
+		return buttonTrigger;
+	}
+	
+	public Button GetButtonThumb(){
+		return buttonThumb;
+	}
+	
+	public Button GetButton5(){
+		return button5;
+	}
+
+	public Button GetButton6(){
+		return button6;
+	}
+	
+	public Button GetButton7(){
+		return button7;
+	}
+	
+	public Button GetButton8(){
+		return button8;
+	}
+	
+	public Button GetButton9(){
+		return button9;
+	}
+	
+	public Button GetButton10(){
+		return button10;
+	}
+	
+	public Button GetButton11(){
+		return button11;
+	}
+	
+	public Button GetButton12(){
+		return button12;
+	}
+	
 	
 	//returns joystick X axis
 	public double Get_X_Axis() {
